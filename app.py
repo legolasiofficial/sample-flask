c6b942af545e02b22b5f5c3bd9bd2bd606894987
legolasiofficial <121556397+legolasiofficial@users.noreply.github.com>
2022-12-28 15:48:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,18 @@
-import os
-from flask import Flask, render_template, request, url_for, redirect
+import flask
+from flask import Flask
+from flask_wp import FlaskWP
 from flask_sqlalchemy import SQLAlchemy
 
-from sqlalchemy.sql import func
-
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://doadmin:@dbaas-db-5470089-do-user-13178331-0.b.db.ondigitalocean.com:25060/defaultdb'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
+app.config['SQLALCHEMY_BINDS'] = {
+    "wordpress" : "mysql://doadmin:@dbaas-db-5470089-do-user-13178331-0.b.db.ondigitalocean.com:25060/defaultdb"
+}
 db = SQLAlchemy(app)
+fwp = FlaskWP(db, app)
 
+@app.route("/")
+def index():
+    return flask.jsonify([{'user_email': usr.user_email} for usr in fwp.User.query.all()])
 
-class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(100), nullable=False)
-    lastname = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(80), unique=True, nullable=False)
-    age = db.Column(db.Integer)
-    created_at = db.Column(db.DateTime(timezone=True),
-                           server_default=func.now())
-    bio = db.Column(db.Text)
-
-    def __repr__(self):
-        return f'<Student {self.firstname}>'
+if __name__ == "__main__":
+    app.run(debug=True)
